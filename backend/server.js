3b0a1db18ee52b77a3196f664e8499d8e683fa97
ShahriarNiloy <shahariarniloyblv@gmail.com>
2022-08-30 10:11:51 +0600

    passport js config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const passportConfig = require("./passport/passportConfig");
+const authRoutes = require("./routes/authRoutes");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 app.use(
@@ -23,6 +26,8 @@ app.use(
   })
 );
 
+app.use("/auth", authRoutes);
+
 app.listen("5000", () => {
-  console.log("listening from port 5000 ");
+  console.log("listening on port 5000");
 });
